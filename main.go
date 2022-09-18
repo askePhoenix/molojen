46cb8f1ba98e7e5a965d3b3e66ec571bc12208f0
askePhoenix <aistarteverything@gmail.com>
2022-09-19 07:02:41 +0900

    메모

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+	// type
+	// int == i
+	// string == s
+	// float32 == f32
+	// float64 == f64
+
+	// custom/.molojen
+	// ii>>s/m,t,f,r,e       <- map, flat-map, filter, reduce, for-each
+
+	// package common
+	// type iArr []int
+	// type ii struct { i1 iArr, i2 iArr }
+	// type sArr []string
+	// type s struct { s sArr }
+
+	// << common mo _ m,t,f,r,e generator >>
+
+	// package custom
+	// type mo1 struct { col common.ii }
+	// type param1 struct { e1 int, e2 int }
+	// [int, int, string] -> func( param1 ) string
+
+	// .molojen
+	// ii>>s
+	// ::/custom/sumTwoIntConvertString
+
+	// ii_s.go package = ii_s
+	//
+
+	//
+	//
+}
