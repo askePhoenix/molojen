bf9be90e8462e81c18d31291d686e287a5e3707a
askePhoenix <aistarteverything@gmail.com>
2022-09-28 21:17:55 +0900

    add toIntArray

--- lo_test/fn/func.string.go
+++ lo_test/fn/func.string.go
@@ -6,6 +6,14 @@ func IntToString(i int, _ int) string {
 	return strconv.Itoa(i)
 }
 
+func StringToInt(s string, _ int) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+	return i
+}
+
 func Concat(i string, _ int) string {
 	return i + " - test"
 }
--- lo_test/main.go
+++ lo_test/main.go
@@ -11,21 +11,20 @@ type FuncIntArrayConsumer func(int, int)
 
 func (i IntArray) Map(f FuncIntArray) IntArray    { return lo.Map[int, int](i, f) }
 func (i IntArray) ForEach(f FuncIntArrayConsumer) { lo.ForEach[int](i, f) }
-func (i IntArray) ToStringList() StringArray      { return lo.Map[int, string](i, fn.IntToString) }
+func (i IntArray) ToStringArray() StringArray     { return lo.Map[int, string](i, fn.IntToString) }
 
 type StringArray []string
 type FuncStringArray func(string, int) string
 type FuncStringArrayConsumer func(string, int)
 
-func (i StringArray) Map(f FuncStringArray) StringArray { return lo.Map[string, string](i, f) }
-func (i StringArray) ForEach(f FuncStringArrayConsumer) { lo.ForEach[string](i, f) }
+func (s StringArray) Map(f FuncStringArray) StringArray { return lo.Map[string, string](s, f) }
+func (s StringArray) ForEach(f FuncStringArrayConsumer) { lo.ForEach[string](s, f) }
+func (s StringArray) ToIntArray() IntArray              { return lo.Map[string, int](s, fn.StringToInt) }
 
 func main() {
 	test := IntArray{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	test.
-		Map(fn.FuncMultiple2).
-		Map(fn.FuncMultiple2).
-		ToStringList().
+		ToStringArray().
 		Map(fn.Concat).
 		ForEach(fn.FuncPrintString)
 }
