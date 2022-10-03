0bb923471999878816ba493f61331625538b9ca8
askePhoenix <aistarteverything@gmail.com>
2022-10-03 23:43:45 +0900

    generic 2

--- jen_test/main.go
+++ jen_test/main.go
@@ -31,9 +31,57 @@ func main() {
 		Id("IntArray")).Id("ForEach").
 		Params(jen.Id("f").Id("FuncIntArrayConsumer")).
 		Block(
-			jen.Qual("github.com/samber/lo", "Map").
-				Types(jen.Int(), jen.Int()).Call(jen.Id("i"), jen.Id("f")),
+			jen.Qual("github.com/samber/lo", "ForEach").
+				Types(jen.Int()).Call(jen.Id("i"), jen.Id("f")),
 		)
+
+	f.
+		Func().Params(jen.Id("i").
+		Id("IntArray")).Id("ToStringArray").
+		Params().
+		Id("StringArray").Block(
+		jen.Return(
+			jen.Qual("github.com/samber/lo", "Map").
+				Types(jen.Int(), jen.String()).Call(jen.Id("i"),
+				jen.Qual("github.com/askePhoenix/molojen/lo_test/fn", "IntToString")),
+		),
+	)
+
+	f.Type().Id("StringArray").Index().String()
+	f.Type().Id("FuncStringArray").Func().Params(jen.String(), jen.Int()).String()
+	f.Type().Id("FuncStringArrayConsumer").Func().Params(jen.String(), jen.Int())
+
+	f.
+		Func().Params(jen.Id("s").
+		Id("StringArray")).Id("Map").
+		Params(jen.Id("f").Id("FuncStringArray")).
+		Id("StringArray").Block(
+		jen.Return(
+			jen.Qual("github.com/samber/lo", "Map").
+				Types(jen.String(), jen.String()).Call(jen.Id("s"), jen.Id("f")),
+		),
+	)
+
+	f.
+		Func().Params(jen.Id("s").
+		Id("StringArray")).Id("ForEach").
+		Params(jen.Id("f").Id("FuncStringArrayConsumer")).
+		Block(
+			jen.Qual("github.com/samber/lo", "ForEach").
+				Types(jen.Int()).Call(jen.Id("s"), jen.Id("f")),
+		)
+
+	f.
+		Func().Params(jen.Id("s").
+		Id("StringArray")).Id("ToStringArray").
+		Params().
+		Id("IntArray").Block(
+		jen.Return(
+			jen.Qual("github.com/samber/lo", "Map").
+				Types(jen.String(), jen.Int()).Call(jen.Id("s"),
+				jen.Qual("github.com/askePhoenix/molojen/lo_test/fn", "StringToInt")),
+		),
+	)
 
 	buf := &bytes.Buffer{}
 
--- jen_test/test/generated.go
+++ jen_test/test/generated.go
@@ -1,6 +1,9 @@
 package main
 
-import lo "github.com/samber/lo"
+import (
+	fn "github.com/askePhoenix/molojen/lo_test/fn"
+	lo "github.com/samber/lo"
+)
 
 type IntArray []int
 type FuncIntArray func(int, int) int
@@ -10,5 +13,22 @@ func (i IntArray) Map(f FuncIntArray) IntArray {
 	return lo.Map[int, int](i, f)
 }
 func (i IntArray) ForEach(f FuncIntArrayConsumer) {
-	lo.Map[int, int](i, f)
+	lo.ForEach[int](i, f)
+}
+func (i IntArray) ToStringArray() StringArray {
+	return lo.Map[int, string](i, fn.IntToString)
+}
+
+type StringArray []string
+type FuncStringArray func(string, int) string
+type FuncStringArrayConsumer func(string, int)
+
+func (s StringArray) Map(f FuncStringArray) StringArray {
+	return lo.Map[string, string](s, f)
+}
+func (s StringArray) ForEach(f FuncStringArrayConsumer) {
+	lo.ForEach[int](s, f)
+}
+func (s StringArray) ToStringArray() IntArray {
+	return lo.Map[string, int](s, fn.StringToInt)
 }
